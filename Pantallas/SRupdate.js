69f069fe6a80dd6daf79bfc77875a9d94524cf03
ShadowGarrido <draymonluna@gmail.com>
2022-07-13 18:10:45 -0500

    inputs con recopilacion

--- Pantallas/SRupdate.js
+++ Pantallas/SRupdate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState} from 'react'
-import {ScrollView, Button, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import {ScrollView, Button, View, StyleSheet, Text, TouchableOpacity, TextInput } from 'react-native';
 import { db } from '../Server/Conexion';
 import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 
@@ -23,10 +23,32 @@ const SRupdate = () => {
         Subtitulo:{
             fontSize: 25,
         },
+        CajasT:{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        input: {
+            height: 40,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+          },
       });
 
 
     const [elementos, setelementos]=useState([])
+    const [productos, setproductos]=useState({
+        producto:'',
+        precio:'',
+        existencia:'',
+        categoria:''
+      })
+
+    const capturar =(atrib,valor) =>{
+        setproductos({...productos,[atrib]:valor})
+    }  
+
 
     async function leer(){
         const querySnapshot = await getDocs(collection(db, "Productos"));
@@ -50,6 +72,39 @@ const SRupdate = () => {
     return (
         <ScrollView style={styles.Sec}>
         <Text  style={styles.Titulo} >Actualizar información</Text>
+
+        <View style={styles.CajasT}>
+
+        <TextInput
+        style={styles.input}
+        placeholder="Producto"
+        value={productos.producto}
+        onChangeText={(value)=>capturar('producto',value)}
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Precio"
+        value={productos.precio}
+        onChangeText={(value)=>capturar('precio',value)}
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Existencia"
+        value={productos.existencia}
+        onChangeText={(value)=>capturar('existencia',value)}
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder="Categoria"
+        value={productos.categoria}
+        onChangeText={(value)=>capturar('categoria',value)}
+        />
+        
+        </View>
+
         <Button title="Leer"  onPress={() =>leer()}>Cargar Productos</Button>
         {
         elementos.map(elemento=>{
